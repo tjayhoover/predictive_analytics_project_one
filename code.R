2427504e5ef0d51974743340b1487fcb31ddf137
ajrilatt <ajrilatt@gmail.com>
2024-03-20 10:59:57 -0400

    first descriptive output

--- code.R
+++ code.R
@@ -1,3 +1,17 @@
 # Adam Rilatt and Tyler Hoover
 # 20 March 2024
 # Predictive Analytics Group Project 1
+
+library(dplyr)
+
+load("classroom6.RData")
+
+summary(classroom6)
+
+# a.
+classroom6 %>%
+  group_by(sex) %>%
+  summarize(Mean = mean(mathgain),
+            SD = sd(mathgain))
+
+# TODO: More of these for level 1, then for level 2
